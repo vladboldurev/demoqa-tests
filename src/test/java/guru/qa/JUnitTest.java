317ef4ea196deea44280d19da352167e9e6fed77
Владислав Болдырев <vboldyrev@uma.tech>
2022-08-21 21:14:11 +0300

    added small refactor and add logger slf4j

--- src/test/java/guru/qa/JUnitTest.java
+++ src/test/java/guru/qa/JUnitTest.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-public class SimpleTest {
+public class JUnitTest {
 
     @BeforeEach
     void setUp() {
---
+++ src/test/java/guru/qa/TextBoxTests.java
@@ -0,0 +1,13 @@
+package guru.qa;
+
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.open;
+
+public class TextBoxTests {
+
+    @Test
+    void fillFormTest() {
+        open("https://demoqa.com/text-box");
+    }
+}
